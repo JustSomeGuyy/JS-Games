8f04ae5118ed214e20f48d46390f1605cd8a2be7
Tyler Wojciechowski <106572294+JustSomeGuyy@users.noreply.github.com>
2023-01-26 12:54:55 -0600

    finalized the JavaScript code for keeping score, and confirming matching/non-matching cards.

--- Memory_Game/main.js
+++ Memory_Game/main.js
@@ -52,6 +52,7 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector('#grid');
+const resultDisplay = document.querySelector('#result');
 let cardsChosen = [];
 let cardsChosenIds = [];
 const cardsWon = [];
@@ -69,17 +70,33 @@ createBoard();
 
 function checkMatch(){
     const cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenIds[0];
+    const optionTwoId = cardsChosenIds[1];
+    if( optionOneId == optionTwoId){
+        cards[optionOneId].setAttribute('src', 'images/blank.png');
+        cards[optionTwoId].setAttribute('src', 'images/blank.png');
+        alert('You have clicked the same image!')
+    }
 
-    console.log('check for match!');
     if(cardsChosen[0] == cardsChosen[1]){
         alert('you found a match!');
-        cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png');
-        cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png');
-        cards[cardsChosenIds[0]].removeEventListener('click', flipCard);
-        cards[cardsChosenIds[1]].removeEventListener('click', flipCard);
+        cards[optionOneId].setAttribute('src', 'images/white.png');
+        cards[optionTwoId].setAttribute('src', 'images/white.png');
+        cards[optionOneId].removeEventListener('click', flipCard);
+        cards[optionTwoId].removeEventListener('click', flipCard);
         cardsWon.push(cardsChosen);
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/blank.png');
+        cards[optionTwoId].setAttribute('src', 'images/blank.png');
+        alert('Sorry, try again!');
     }
+    resultDisplay.textContent = cardsWon.length;
     cardsChosen = [];
+    cardsChosenIds = [];
+
+    if(cardsWon.length == cardArray.length/2){
+        resultDisplay.textContent = 'Congratulations, you found them all!'
+    }
 }
 
 function flipCard(){
